354377faaa0620a213ee1edb9d3509cb17b1edcd
divineion <najima.guermoudi@gmail.com>
2025-10-02 15:57:26 +0200

    Initial commit
    Initialize Spring Boot Gateway with Reactive Gateway and Spring Security

---
+++ src/main/java/com/medilabo/abernathyclinic/gateway/AbernathyclinicGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.abernathyclinic.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbernathyclinicGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbernathyclinicGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/abernathyclinic/gateway/AbernathyclinicGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.abernathyclinic.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbernathyclinicGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
