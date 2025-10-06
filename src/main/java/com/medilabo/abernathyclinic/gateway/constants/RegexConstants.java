501025bf3c0893733b3d2de4da6c07aac55b3f8e
divineion <najima.guermoudi@gmail.com>
2025-10-06 09:21:01 +0200

    feat(constants): add RegexConstants class with UUID_PATTERN regex

---
+++ src/main/java/com/medilabo/abernathyclinic/gateway/constants/RegexConstants.java
@@ -0,0 +1,7 @@
+package com.medilabo.abernathyclinic.gateway.constants;
+
+public class RegexConstants {
+	private RegexConstants(){}
+	
+	public static final String UUID_PATTERN = "(?<uuid>[a-fA-F0-9\\-]{36})";
+}
