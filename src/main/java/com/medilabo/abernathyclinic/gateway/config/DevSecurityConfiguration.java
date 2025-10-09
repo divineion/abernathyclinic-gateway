1444f86d84c00c74b0aa9ecace3c4c5076e857ea
divineion <najima.guermoudi@gmail.com>
2025-10-09 22:27:13 +0200

    feat(config): expose a PasswordEncoder bean in DevSecurityConfiguration

--- src/main/java/com/medilabo/abernathyclinic/gateway/config/DevSecurityConfiguration.java
+++ src/main/java/com/medilabo/abernathyclinic/gateway/config/DevSecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -12,5 +14,10 @@ public class DevSecurityConfiguration {
     @Bean
     SecurityWebFilterChain filterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {
         return serverHttpSecurity.authorizeExchange(exchange -> exchange.anyExchange().permitAll()).build();
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
     }
 }
