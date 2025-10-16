023938233501601c6a98ec564ecd64afde31ae92
divineion <najima.guermoudi@gmail.com>
2025-10-16 21:31:44 +0200

    feat(auth): implement http basic authentication
    Configure ServerHttpSecurity :
     - "/login" path is on public access,
     - for any other path, an authentication is required.
    
    Enable Basic Authentication using `httpBasic()`.

--- src/main/java/com/medilabo/abernathyclinic/gateway/config/DevSecurityConfiguration.java
+++ src/main/java/com/medilabo/abernathyclinic/gateway/config/DevSecurityConfiguration.java
@@ -3,17 +3,28 @@ package com.medilabo.abernathyclinic.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+
 @Configuration
 @Profile("dev")
 public class DevSecurityConfiguration {
-    @Bean
-    SecurityWebFilterChain filterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {
-        return serverHttpSecurity.authorizeExchange(exchange -> exchange.anyExchange().permitAll()).build();
+
+	@Bean
+	SecurityWebFilterChain filterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {   	
+    	return serverHttpSecurity
+        		// configurer les autorisations
+        		.authorizeExchange(exchange -> exchange
+        			.pathMatchers("/login").permitAll()
+        			.anyExchange().authenticated()
+				)
+        		// username / mot de passe
+        		.httpBasic(Customizer.withDefaults())
+	        	.build();
     }
     
     @Bean
