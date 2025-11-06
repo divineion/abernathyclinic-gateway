bd4757ec8800ed52ae63f9a1716a0e0ac7c6137d
divineion <najima.guermoudi@gmail.com>
2025-11-06 05:25:13 +0100

    feat(security): add CustomMapReactiveUserDetailsService
    Implement a custom ReactiveUserDetailsService, based on a Map (for fast
    lookup) to expose AppUserDetails.
    This allows access to specific fields (like ID).

---
+++ src/main/java/com/medilabo/abernathyclinic/gateway/config/CustomMapReactiveUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.medilabo.abernathyclinic.gateway.config;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import reactor.core.publisher.Mono;
+
+public class CustomMapReactiveUserDetailsService implements ReactiveUserDetailsService {
+	// représenter les users
+	Map<String, AppUser> users;
+	
+	// convertir les données dans le constructeur pour retourner un Map de users
+	// plus performant que List pour la recherche
+	public CustomMapReactiveUserDetailsService(List<AppUser> appUsers) {
+		this.users = appUsers.stream()
+				.collect(Collectors.toMap(AppUser::getUsername, Function.identity()));
+	}
+	
+	@Override
+	public Mono<UserDetails> findByUsername(String username) {
+		AppUser user = users.get(username.toLowerCase());
+		
+		if (user == null) {
+			return Mono.error(new UsernameNotFoundException("user " + username + " not found"));
+		}
+		
+		return Mono.just(user);
+	}
+}
