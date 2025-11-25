d4a72fd2dfb24a64d0b483a9ec5a56749c2cb956
divineion <najima.guermoudi@gmail.com>
2025-11-25 03:46:19 +0100

    feat(docker): remove MicroServiceUriConstants
    These constants are no longer necessary (microservices URLs are now
    defined in environment variables)

--- src/main/java/com/medilabo/abernathyclinic/gateway/constants/MicroservicesUriConstants.java
+++
@@ -1,9 +0,0 @@
-package com.medilabo.abernathyclinic.gateway.constants;
-
-public final class MicroservicesUriConstants {
-	private MicroservicesUriConstants(){}
-	
-	public final static String MICROSERVICE_PATIENT_URI = "http://localhost:8081";
-	public final static String MICROSERVICE_NOTE_URI = "http://localhost:8083";
-	public static final String MICROSERVICE_REPORT_URI = "http://localhost:8084";
-}
